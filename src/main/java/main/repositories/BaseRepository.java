2e40f06092259927559f88b8df5c6c230d1a8238
Mevlut Arslan <mevlutcaferarslan@outlook.com>
2021-04-15 13:12:12 +0200

    Implement an interface for standardizing database operations

---
+++ src/main/java/main/repositories/BaseRepository.java
@@ -0,0 +1,16 @@
+package main.repositories;
+
+import main.utils.NotFoundException;
+
+import java.util.List;
+
+public interface BaseRepository<T> {
+    T findByName(String name) throws NotFoundException;
+
+    void add(T course);
+
+    void remove(T course);
+
+    void update(T instanceToUpdate);
+
+}
